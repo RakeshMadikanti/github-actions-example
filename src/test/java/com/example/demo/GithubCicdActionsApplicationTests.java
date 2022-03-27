4916b9548921cf5a9563016acb662f7d5aeea76d
RakeshMadikanti <rakesh.madikanti@gmail.com>
2022-03-28 01:36:42 +0530

    code

--- src/test/java/com/example/demo/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/example/demo/GithubCicdActionsApplicationTests.java
@@ -19,3 +19,12 @@ class GithubCicdActionsApplicationTests {
 	}
 
 }
+
+
+//echo "# github-actions-example" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/RakeshMadikanti/github-actions-example.git
+//git push -u origin main
